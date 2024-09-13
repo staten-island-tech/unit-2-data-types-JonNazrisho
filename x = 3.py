6cb767dcddbc85afa50bcee959ef6eb3a1a1af3d
JonNazrisho <jnazrisho@gmail.com>
2024-09-13 09:22:40 -0400

    Update x = 3.py

--- x = 3.py
+++ x = 3.py
@@ -1,7 +1,19 @@
-x = 3
+""" x = 3
 y = float(3)
 print(x,y)
 values = [1,2.23,5,7,2,30,15]
 print(values)
 for i in values:
-    print(i)
+    print(values[0])
+    print(values[7]) """
+""" day_of_the_week = input("What day is it?")
+if day_of_the_week == "Friday":
+    print("correct")
+else:
+    print("incorrect") """
+
+time = input('what time is it?')
+if time == '9:23':
+    print('Right')
+else:
+    print('wrong')
